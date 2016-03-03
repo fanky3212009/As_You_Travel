fa3f47a56f9c0f7de1d38047eec94851715d40bc
Franky Chau <houun.chau@gmail.com>
2016-03-03 17:28:46 -0500

    Refactor application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
-
   def load_commentable
     @klass = eval(controller_name.classify)
     @commentable = @klass.find(params[:id])
-    # @commentable_comments_path
-    # comments_path(:commentable_type => @commentable.class, :commentable_id => @commentable.id)
   end
 end
