1046498b745c85c0891db1e67d6ac5b33ac0558b
Qirui Sun <qiruisun@gmail.com>
2016-03-05 11:51:37 -0500

    Add data-no-turbolinks to the journey links

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,12 +2,9 @@ class PasswordResetsController < ApplicationController
   def create
 
     @user = User.find_by_email(params[:email])
-
     @user.deliver_reset_password_instructions! if @user
 
-
     redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
-
 
   end
 
