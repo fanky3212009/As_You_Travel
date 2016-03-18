df3339e18555fc2af13646f8fe517d907be83317
Franky Chau <houun.chau@gmail.com>
2016-03-17 20:37:09 -0400

    Join assets paths for vendor/assets

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module FinalProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join('vendor', 'assets')
     config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "fonts")
   end
 end
