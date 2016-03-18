f0402c6372372c5a77a8a4428a8c36accfeab187
Qirui Sun <qiruisun@gmail.com>
2016-03-18 17:33:11 -0400

    Adjust the sign up routes and apply auto-login after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        auto_login(@user)
         format.html { redirect_to root_url, notice: 'User was successfully created.' }
         format.json { render :home, status: :created, location: @user }
       else
